f4cba29a5f961e58b80df5758385c3bb03e98d0c
lzmoos <leo.zmoos@heig-vd.ch>
2023-11-10 17:33:40 +0100

    DatePicker

--- Labo3/app/src/main/java/ch/heigvd/daa/labo3/MainActivity.kt
+++ Labo3/app/src/main/java/ch/heigvd/daa/labo3/MainActivity.kt
@@ -1,8 +1,20 @@
 package ch.heigvd.daa.labo3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import ch.heigvd.daa.labo3.databinding.ActivityMainBinding
+import ch.heigvd.daa.labo3.model.Person
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointBackward
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
+import java.sql.Date
+import java.text.SimpleDateFormat
+
+import java.util.Locale
+import java.util.TimeZone
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +23,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.imageButtonCake.setOnClickListener {
-            //TODO : Material Datepicker
+
+        binding.imageButtonCake.setOnClickListener() {
+            datePicker();
+        }
+    }
+
+    /**
+     * Cette fonction est utilisée pour le choix de la date de naissance
+     */
+    fun datePicker()
+    {
+        // Bloquer les dates dans le futur pour les dates de naissance
+        val constraintsBuilder = CalendarConstraints.Builder().setValidator(DateValidatorPointBackward.now())
+
+        val datePicker = MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .setCalendarConstraints(constraintsBuilder.build())
+            .build()
+
+        datePicker.show(supportFragmentManager, "DatePicker")
+
+        // Evénement quand on clique sur le bouton OK
+        datePicker.addOnPositiveButtonClickListener() {
+            // Formattage de la date
+            val date = Person.dateFormatter.format(Date(it))
+            binding.editTextBirthdate.setText(date)
+
         }
     }
 
